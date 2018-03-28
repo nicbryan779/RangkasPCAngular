c86e6205521d9039c596b989f477a0a1b5075f52
Johanes <Jron@Johaness-MacBook-Pro.local>
2018-03-28 22:00:36 +0700

    Install jQuery, uses bootstrap & create checkout page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule
