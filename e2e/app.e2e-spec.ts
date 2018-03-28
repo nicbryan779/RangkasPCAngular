6d1da2d94a3a7fa9636ae7690cbae147a015eb2e
Johanes <Jron@Johaness-MacBook-Pro.local>
2018-03-28 11:50:21 +0700

    First

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('rangkas-pcangular App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
