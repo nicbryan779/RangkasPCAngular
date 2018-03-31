5f2a0a71517ef985924049746487f6eb92a291a5
johanesr <johanesronaldo@gmail.com>
2018-03-31 21:38:27 +0700

    Finish setup & seperates homepage layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomepageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
