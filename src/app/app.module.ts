b78e14279f053879f5a4e0007128f2f48a4fe00a
Johanes <Jron@Johaness-MacBook-Pro.local>
2018-03-31 21:10:49 +0700

    Seperates Homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { CheckoutComponent } from './checkout/checkout.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CheckoutComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule
