60cc966502ea9dd9314b374915c8a5a79bad2eac
johanesr <johanesronaldo@gmail.com>
2018-04-08 01:48:03 +0700

    Enable Login and Logout Button Change

--- src/app/header/navbar/navbar.component.ts
+++ src/app/header/navbar/navbar.component.ts
@@ -9,30 +9,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  // isLogin: boolean;
-
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
-    // this.checkLogin();
   }
-
-  // checkLogin() {
-  //   if (localStorage.getItem('token')) {
-  //     console.log(localStorage.getItem('token'));
-  //     this.isLogin = true;
-  //     console.log(this.isLogin);
-  //   } else {
-  //     this.isLogin = false;
-  //   }
-  // }
 
   submitLogout() {
     this.authService.logout();
     alert('You have successfully logged out!');
     this.router.navigateByUrl('/home');
-    // this.isLogin = false;
   }
 
 }
